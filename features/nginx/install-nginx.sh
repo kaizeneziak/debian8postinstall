8bbe43c5d173451e6a003a68804492033017d16f
root <root@test-web>
2016-10-14 19:03:36 +0200

    Mise à jour du fichier install-nginx.sh

--- features/nginx/install-nginx.sh
+++ features/nginx/install-nginx.sh
@@ -8,17 +8,19 @@ PATH_DEBIAN8POSTINSTALL=$1
 LOG_FILE=$2
 
 # Importation des variables
-. $PATH_DEBIAN8POSTINSTALL/var/debian8postinstall
-. $PATH_DEBIAN8POSTINSTALL/var/nginx
+. $PATH_DEBIAN8POSTINSTALL/variables # Importation des variables de haut niveau
+. $PATH_FEATURE_NGINX/variables # Importation des variables propres à NGINX
+
 # Importation des fonctions
-. $PATH_DEBIAN8POSTINSTALL/functions/colors.sh
-. $PATH_DEBIAN8POSTINSTALL/functions/generals.sh
+. $PATH_DEBIAN8POSTINSTALL/functions/functions_colors.sh
+. $PATH_DEBIAN8POSTINSTALL/functions/functions_affichage.sh
+. $PATH_DEBIAN8POSTINSTALL/functions/functions_execution.sh
 
 # PROGRAMME
-function_title "Installation de NGINX"
+aff_titre "Installation de NGINX"
 # Importation de la clé GPG permettant de vérifier l'intégrité des fichiers qui seront téléchargés.
-function_import_gpg_url "$NGINX_GPG"
+exec_import_gpg_apt "$NGINX_GPG"
 # Ajout du dépôt STABLE de NGINX.
-function_add_depot "$NGINX_SOURCES_LIST"
+exec_apt_add_depot "$NGINX_SOURCES_LIST"
 # Installer NGINX
-function_apt_install "$NGINX"
+exec_apt_install_uniq "$NGINX"
