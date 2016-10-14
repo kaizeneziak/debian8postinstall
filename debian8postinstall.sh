7cdd7b83208f5963684d88e38243b91ae38dd2fe
root <root@test-web>
2016-10-14 19:01:00 +0200

    Décommenter install-nginx.sh & config-nginx.sh

--- debian8postinstall.sh
+++ debian8postinstall.sh
@@ -5,17 +5,19 @@
 #
 
 # Importation des variables
-. var/debian8postinstall
-. functions/colors.sh
-. functions/generals.sh
+. variables
+. functions/functions_colors.sh
+. functions/functions_affichage.sh
+. functions/functions_execution.sh
+. functions/functions_logs.sh
 
 # Prérequis
 clear
-function_logs
-function_title "Installation des prérequis"
-function_apt_install "$APT_WHIPTAIL"
+logs
+aff_titre "Installation des prérequis"
+exec_apt_install_uniq "$APT_WHIPTAIL"
 # Programme
-RESULTATS=`whiptail --title "Debian 8 Jessie Post-installation" --checklist \
+RESULTATS=$(whiptail --title "Debian 8 Jessie Post-installation" --checklist \
 "Que souhaitez vous faire ?" 13 71 7 \
 "Nginx" "Installer et configurer NGINX" ON \
 "Php7-fpm" "Installer et configurer PHP7-FPM" ON \
@@ -23,9 +25,8 @@ RESULTATS=`whiptail --title "Debian 8 Jessie Post-installation" --checklist \
 "Mariadb-server" "Installer et configurer Mariadb-Server" ON \
 "Lets-encrypt" "Installer et configurer Let's Encrypt" ON \
 "Varnish" "Installer et configurer Varnish 4.0 Cache" ON \
-"Wordpress" "Installer et configurer Wordpress" OFF 3>&1 1>&2 2>&3`
+"Wordpress" "Installer et configurer Wordpress" OFF 3>&1 1>&2 2>&3)
 
-#function_traitement "$RESULTATS"
 for RESULTAT in $(echo $RESULTATS | sed 's/\"//g' | tr ' ' '\n');
 do
 	case "$RESULTAT" in
@@ -34,29 +35,29 @@ do
 			bash $SCRIPT_CONFIG_NGINX $PATH_CURRENT $LOG_FILE
 		;;
 		"Php7-fpm") 
-                        bash $SCRIPT_INSTALL_PHP7FPM $PATH_CURRENT $LOG_FILE
+                        #bash $SCRIPT_INSTALL_PHP7FPM $PATH_CURRENT $LOG_FILE
                         #bash $SCRIPT_CONFIG_PHP7FPM $PATH_CURRENT $LOG_FILE
 		;;
 		"Mariadb-client") 
-                        bash $SCRIPT_INSTALL_MARIADB_CLIENT $PATH_CURRENT $LOG_FILE
+                        #bash $SCRIPT_INSTALL_MARIADB_CLIENT $PATH_CURRENT $LOG_FILE
                         #bash $SCRIPT_CONFIG_MARIADB_CLIENT $PATH_CURRENT $LOG_FILE
 		;;
                 "Mariadb-server") 
-                        bash $SCRIPT_INSTALL_MARIADB_SERVER $PATH_CURRENT $LOG_FILE
+                        #bash $SCRIPT_INSTALL_MARIADB_SERVER $PATH_CURRENT $LOG_FILE
                         #bash $SCRIPT_CONFIG_MARIADB_SERVER $PATH_CURRENT $LOG_FILE
 		;;
                 "Lets-encrypt") 
-                        bash $SCRIPT_INSTALL_LETSENCRYPT $PATH_CURRENT $LOG_FILE
+                        #bash $SCRIPT_INSTALL_LETSENCRYPT $PATH_CURRENT $LOG_FILE
                         #bash $SCRIPT_CONFIG_LETSENCRYPT $PATH_CURRENT $LOG_FILE
 		;;
                 "Varnish") 
-                        bash $SCRIPT_INSTALL_VARNISH $PATH_CURRENT $LOG_FILE
+                        #bash $SCRIPT_INSTALL_VARNISH $PATH_CURRENT $LOG_FILE
                         #bash $SCRIPT_CONFIG_VARNISH $PATH_CURRENT $LOG_FILE
                         #bash $SCRIPT_CONFIG_VARNISH_FIREWALL $PATH_CURRENT $LOG_FILE
                         #bash $SCRIPT_CONFIG_VARNISH_DDOS $PATH_CURRENT $LOG_FILE
 		;;
                 "Wordpress") 
-                        bash $SCRIPT_INSTALL_WORDPRESS $PATH_CURRENT $LOG_FILE
+                        #bash $SCRIPT_INSTALL_WORDPRESS $PATH_CURRENT $LOG_FILE
                         #bash $SCRIPT_CONFIG_WORDPRESS $PATH_CURRENT $LOG_FILE
 		;;
 	esac
