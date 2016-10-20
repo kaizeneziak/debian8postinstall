d5330ebbcb46b72d5dc57bdcacd2cec5e1f5ee04
root <root@test-web>
2016-10-20 17:38:06 +0200

    Add sudo su -c à la fonction exec_sed_mariadb

--- functions/functions_execution.sh
+++ functions/functions_execution.sh
@@ -362,7 +362,7 @@ exec_sed_mariadb () {
         MODIFICATION=$2
         FILE=$FILE_MARIADB_MY_CNF
 
-        exec_command "sudo sed -i -e 's/$MOT/$MODIFICATION/g' $FILE"
+        exec_command "sudo su -c 'sed -i -e \"s/$MOT/$MODIFICATION/g\" $FILE'"
         if [ $? -eq 0 ]; then
                 aff_message "ok" "Remplacement de l'expression $(aff_important $1) par $(aff_important $2) dans le fichier $($FILE)"
         else
