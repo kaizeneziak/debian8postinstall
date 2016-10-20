6e1acfef1ed169bbfe9438b5183facb43b5df733
root <root@test-web>
2016-10-20 17:08:25 +0200

    Script installation du client Mariadb

--- features/mariadb/install-mariadb-client.sh
+++ features/mariadb/install-mariadb-client.sh
@@ -8,17 +8,101 @@
 PATH_DEBIAN8POSTINSTALL=$1
 LOG_FILE=$2
 
+# Importation des variables
+. $PATH_DEBIAN8POSTINSTALL/variables # Importation des variables de haut niveau
+. $PATH_FEATURE_MARIADB/variables # Importation des variables propres à mariadb
+
 # Importation des fonctions
-. $PATH_DEBIAN8POSTINSTALL/var/debian8postinstall
-. $PATH_DEBIAN8POSTINSTALL/functions/colors.sh
-. $PATH_DEBIAN8POSTINSTALL/functions/generals.sh
+. $PATH_DEBIAN8POSTINSTALL/functions/functions_colors.sh
+. $PATH_DEBIAN8POSTINSTALL/functions/functions_affichage.sh
+. $PATH_DEBIAN8POSTINSTALL/functions/functions_execution.sh
+. $PATH_DEBIAN8POSTINSTALL/functions/functions_test.sh
 
 # PROGRAMME
-function_title "Installation de MARIADB-CLIENT"
-#function_root
-#function_apt_update
-#function_apt_install "$APT_PREREQUIS"
-#function_import_gpg_apt "$GPG_MARIADB"
-#function_add_depot "$DEPOT_MARIADB" "$SOURCES_LIST_MARIADB"
-#function_apt_install "$APT_MARIADB_CLIENT"
+
+aff_titre "Installation de MariaDB Client"
+# Install Prérequis
+exec_apt_install_prerequis "$VAR_APT_MARIADB_PREREQUIS"
+# Import de la clé GPG
+exec_import_gpg_apt "$VAR_GPG_MARIADB"
+# Ajouter le dépôt mariadb.list
+exec_copy_file "$FILE_MARIADB_DEPOT_SRC" "$FILE_MARIADB_DEPOT_DST"
+# Mise à jour des paquets
+exec_apt_update
+# Installation du client MariaDB
+exec_apt_install_uniq "$VAR_APT_MARIADB_CLIENT"
+
+aff_titre "Configuration de MariaDB Client"
+# WHIPTAIL : Adresse IP du serveur ?
+while true
+do
+        MARIADB_SERVER_IP=$(whiptail --title " Configuration du client MariaDB " --inputbox "\nQuel est l'adresse IP du serveur MariaDB ?" 9 50 3>&1 1>&2 2>&3)
+        if [ $? -eq 1 ]; then
+                exit 1
+        fi
+	if [ ! -z $MARIADB_SERVER_IP ]; then
+		if [[ $MARIADB_SERVER_IP =~ $REGEX_IP_ADDRESS ]]; then
+                	break
+               	else
+                	whiptail --title "Erreur !" --msgbox "\nVeuillez saisir une adresse IP !" 8 37
+                fi
+	fi
+done
+exec_sed_mariadb "$VAR_SQL_MARIADB_BINDADDRESS" "bind-address = $MARIADB_SERVER_IP"
+
+# WHIPTAIL : Port d'écoute du serveur
+while true
+do
+        MARIADB_SERVER_PORT=$(whiptail --title " Configuration du client MariaDB " --inputbox "\nQuel est le port d'écoute du serveur MariaDB ?" 9 50 3306 3>&1 1>&2 2>&3)
+        if [ $? -eq 1 ]; then
+                exit 1
+        fi
+        if [[ "$MARIADB_SERVER_PORT" =~ ^[0-9]+$ ]]; then
+                break
+        fi
+done
+exec_sed_mariadb "$VAR_SQL_MARIADB_PORT" "$MARIADB_SERVER_PORT"
+
+# TEST CONNEXION
+whiptail --title " Information " --msgbox "\nUn test va être réalisé afin de vérifier que le client mariadb est opérationnel." 9 50
+
+while true
+do
+	# Compte admin mariadb
+	while true
+	do
+	        MARIADB_SERVER_ADMIN=$(whiptail --title " Configuration du client MariaDB " --inputbox "\nVeuillez saisir le login de l'administrateur du serveur MariaDB : " 9 70 secureroot 3>&1 1>&2 2>&3)
+	        if [ $? -eq 1 ]; then
+	                exit 1
+	        fi
+	        if [ ! -z $MARIADB_SERVER_ADMIN ]; then
+	                break
+	        else
+			whiptail --title "Erreur !" --msgbox "\nVeuillez saisir un nom d'utilisateur !" 8 37
+		fi
+	done
+
+	# Mot de passe admin
+	while true
+	do
+		MARIADB_SERVER_PASS=$(whiptail --title " Configuration du client MariaDB " --passwordbox "\nVeuillez saisir le mot de passe de l'administrateur du serveur MariaDB :" 9 77 3>&1 1>&2 2>&3)
+	        if [ $? -eq 1 ];then
+	        	exit 1
+	        fi
+	        if [ ! -z "$MARIADB_SERVER_PASS" ]; then
+	        	break
+	       	else
+	        	whiptail --title "Erreur !" --msgbox "\nVeuillez saisir un mot de passe !" 8 37
+	       	fi
+	done
+
+	# Test
+	test_mariadb "$MARIADB_SERVER_ADMIN" "$MARIADB_SERVER_PASS" "$MARIADB_SERVER_IP" "$MARIADB_SERVER_PORT"
+	if [ $? -eq 0 ]; then
+                whiptail --title " Réussite du test " --msgbox "\nLe client MariaDB est à présent configuré." 8 37
+		break
+	else
+		whiptail --title "Erreur !" --msgbox "Une erreur est survenue !\n\n- Vérifier que l'utilisateur \"$MARIADB_SERVER_ADMIN\" est autorisé à se connecter\nau serveur MariaDB depuis l'adresse IP \"$WEB_SERVER\"\n- Vérifier que l'utilisateur \"$MARIADB_SERVER_ADMIN\" dispose bien des droits d'administrations\n- Si tel n'est pas le cas, exécuter la commande suivante sur le serveur MariaDB :\n\nmysql -u [admin] -p [password] -e \"GRANT ALL PRIVILEGES ON *.* TO '$MARIADB_SERVER_ADMIN'@'$WEB_SERVER' IDENTIFIED BY '[password]' WITH GRANT OPTION; FLUSH PRIVILEGES;\"\n\nEn remplaçant :\n    - [admin] par le login de l'administrateur\n    - [password] par le mot de passe de l'administrateur" 20 95
+	fi
+done
 
