70ce225089165826ab724f6dfd07f3d8f933a183
root <root@test-web>
2016-10-20 15:27:44 +0200

    Modification du menu

--- features/mariadb/install-mariadb-server.sh
+++ features/mariadb/install-mariadb-server.sh
@@ -10,7 +10,7 @@ LOG_FILE=$2
 
 # Importation des variables
 . $PATH_DEBIAN8POSTINSTALL/variables # Importation des variables de haut niveau
-. $PATH_FEATURE_MARIADB/variables # Importation des variables propres à NGINX
+. $PATH_FEATURE_MARIADB/variables # Importation des variables propres à mariadb
 
 # Importation des fonctions
 . $PATH_DEBIAN8POSTINSTALL/functions/functions_colors.sh
@@ -20,9 +20,9 @@ LOG_FILE=$2
 # PROGRAMME
 
 # Whiptail Server local ou distant ?
-CHOIX=$(whiptail --title "Installation de Mariadb Serveur" --menu "Quel type d'installation souhaitez-vous effectuer ?" 10 63 2 \
-"Local" "Installer Mariadb Serveur sur la machine locale" \
-"Remote" "Installer Mariadb Serveur sur un serveur distant" 3>&1 1>&2 2>&3)
+CHOIX=$(whiptail --title "Installation du serveur MariaDB " --menu "\nQuel type d'installation souhaitez-vous effectuer ?" 12 89 2 \
+"Local" "Installe le paquet mariadb-server sur la machine locale" \
+"Remote" "Installe les paquets mariadb-server et mariadb-client sur un serveur distant" 3>&1 1>&2 2>&3)
 
 if [[ $CHOIX == "Local" ]]; then
 	aff_titre "Installation de MariaDB Serveur en local"
@@ -34,19 +34,3 @@ if [[ $CHOIX == "Remote" ]]; then
 else
 	exit 1
 fi
-
-# REMOTE : WHIPTAIL MSG BOX - Nécessite que le serveur distant dispose d'un serveur SSH qui autorise la connexion du compte ROOT
-# REMOTE : Lancer script $SCRIPT_INSTALL_MARIADB_SERVER_REMOTE
-# REMOTE : WHIPTAIL INPUT : Quelle est l'adresse du serveur de base de données distant ? $IP_MARIADB_REMOTE
-# REMOTE : Test SSH root@$IP_MARIADB_REMOTE
-# REMOTE : Si OK --> lancer installation, Si NOK --> exit 1
-# LOCAL : Install Prérequis
-# LOCAL : Import de la clé GPG
-# LOCAL : Ajouter le dépôt mariadb.list
-# LOCAL : Mise à jour des paquets
-# LOCAL : Installer mariadb-server et mariadb-client
-# LOCAL : Lancer mysql_secure_installation --host=$IP_MARIADB_REMOTE --port=$PORT_MARIADB_REMOTE
-# LOCAL : Modifier port d'écoute
-# LOCAL : Modifier le compte root
-
-
